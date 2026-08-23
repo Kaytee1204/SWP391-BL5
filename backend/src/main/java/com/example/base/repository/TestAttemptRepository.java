4cfd028aca81a38c3fcf88d161d1d5f44825f120
khanhndhe <tailankhanhngoc@gmail.com>
2026-08-24 00:24:17 +0700

    fix loi lam bai thi

--- backend/src/main/java/com/example/base/repository/TestAttemptRepository.java
+++ backend/src/main/java/com/example/base/repository/TestAttemptRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TestAttemptRepository extends JpaRepository<TestAttempt, Long> {
-    Page<TestAttempt> findByStudentAccountIdOrderByStartedAtDesc(Long studentId, Pageable pageable);
+    Page<TestAttempt> findByStudentAccountId(Long studentId, Pageable pageable);
     Optional<TestAttempt> findFirstByStudentAccountIdAndQuestionSetQuestionSetIdAndStatusOrderByStartedAtDesc(Long studentId, Long setId, String status);
 }
